92c1f41cd7e0766472804d558e647b7746d8fcd3
simeunchan <81678271+simeunchan@users.noreply.github.com>
2021-04-15 20:22:27 +0900

    Update mytranspose.r

--- mytranspose.r
+++ mytranspose.r
@@ -1,18 +1,15 @@
-
-
-    mytranspose <- function(x) {
-      as.matrix(x)
-      y <- matrix(1, nrow=ncol(x), ncol = nrow(x))
-      for(i in 1:nrow(x)) {
-        for(j in 1:ncol(x)) {
-          y[j,i] <- x[i,j]
-        }
-      }
-      if (is.matrix(x))
-        return(y)
-      else if (is.vector(x))
-        return(as.vector(y)
-      else if (is.data.frame(x))
-        return(as.data.frame(y))
-
-
+mytranspose <- function(x) {
+  as.matrix(x)
+  y <- matrix(1, nrow=ncol(x), ncol = nrow(x))
+  for(i in 1:nrow(x)) {
+    for(j in 1:ncol(x)) {
+      y[j,i] <- x[i,j]
+    }
+  }
+  if (is.matrix(x))
+    return(y)
+  else if (is.vector(x))
+    return(as.vector(y))
+  else if (is.data.frame(x))
+    return(as.data.frame(y))
+}
